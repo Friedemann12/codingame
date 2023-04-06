4b1d31bc47fa7bd5d97f50675a6beae89f682de4
Friedemann12 <julian.friedemann@googlemail.com>
2023-04-06 13:47:57 +0200

    mad-pod-racing

---
+++ mad-pod-racing/mad-pod-racing_Friedemann-Silver.kt
@@ -0,0 +1,201 @@
+/*
+import java.util.*
+import kotlin.math.*
+
+data class Point(val x: Int, val y: Int) {
+    fun toPointDouble(): PointDouble {
+        return PointDouble(this.x.toDouble(), this.y.toDouble())
+    }
+}
+
+data class PointDouble(val x: Double, val y: Double) {
+    fun toPoint(): Point {
+        return Point(this.x.roundToInt(), this.y.roundToInt())
+    }
+}
+
+fun main(args: Array<String>) {
+    val input = Scanner(System.`in`)
+    var boostAvailable = true
+    val path = mutableSetOf<Point>()
+    var pathComplete = false
+    var lap = 1
+    var lastPoint = Point(0, 0)
+    var lastPosition = Point(0, 0)
+
+    var shortestPath: List<Point>? = null
+    var shortestPathMap: MutableMap<Point, Point> = mutableMapOf()
+    var pathPointer = 0
+    // game loop
+    while (true) {
+        val currentPosition = Point(input.nextInt(), input.nextInt())
+        val nextPoint = Point(input.nextInt(), input.nextInt())
+        path.add(nextPoint)
+        if (path.size > 1 && path.first() == nextPoint && lastPoint != nextPoint) {
+            pathComplete = true
+        }
+        if (pathComplete && path.first() == nextPoint && lastPoint != nextPoint) {
+            lap++
+        }
+
+        System.err.println("Path complete: $pathComplete")
+        System.err.println("LAP: $lap")
+
+        val nextCheckpointDist = input.nextInt() // distance to the next checkpoint
+        val nextCheckpointAngle =
+            input.nextInt() // angle between your pod orientation and the direction of the next checkpoint
+        val opponentX = input.nextInt()
+        val opponentY = input.nextInt()
+        var thurst = 100
+
+        //System.err.println("Last PosX: ${lastPosition.x}")
+        //System.err.println("Last PosY: ${lastPosition.y}")
+        //System.err.println("Curr PosX: ${currentPosition.x}")
+        //System.err.println("Curr PosY: ${currentPosition.y}")
+        //System.err.println("Dist: $nextCheckpointDist");
+        //System.err.println("Angle: $nextCheckpointAngle");
+
+        if (lap >= 3 && nextPoint == path.last() && abs(getAngleOfNextTarget(nextCheckpointDist, 400)) <= 3) {
+            println("${shortestPathMap[nextPoint]!!.x} ${shortestPathMap[nextPoint]!!.y} BOOST")
+        } else if (lap >= 2) {
+            if (shortestPath == null) {
+                shortestPath = shortestPath(path.toList(), 400)
+                System.err.println(shortestPath.size)
+                System.err.println(path.size)
+                path.forEachIndexed { i, point ->
+                    shortestPathMap[point] = shortestPath.minBy { distance(point, it) }
+                }
+            }
+            thurst =
+                getThrust(
+                    distance(currentPosition, shortestPathMap[nextPoint]!!).roundToInt(),
+                    getAngleOfNextTarget(nextCheckpointDist, 400)
+                )
+            println("${shortestPathMap[nextPoint]!!.x} ${shortestPathMap[nextPoint]!!.y} $thurst")
+        } else {
+            if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90) {
+                thurst = 0
+            } else thurst = getThrust(nextCheckpointDist, nextCheckpointAngle)
+
+            println("${nextPoint.x} ${nextPoint.y} $thurst")
+        }
+        lastPoint = nextPoint
+        lastPosition = currentPosition
+    }
+}
+
+fun getAngleOfNextTarget(nextCheckpointDist: Int, radius: Int): Int {
+    val angle = Math.toDegrees(asin(radius.toDouble() / nextCheckpointDist.toDouble())).roundToInt()
+    System.err.println("Calculated Angel: $angle")
+    return angle
+}
+
+fun getThrust(dist: Int, angle: Int): Int {
+    return when {
+        dist >= 5000 -> 100
+        dist in 2999 downTo 1000 -> 75
+        dist <= 999 -> {
+            if (angle > 25) 25 else 100
+        }
+
+        else -> 100
+    }
+}
+
+fun getClosestPointOnCircleToPoint(point: PointDouble, circleCenter: PointDouble, radius: Int): Point {
+    val dx = point.x - circleCenter.x
+    val dy = point.y - circleCenter.y
+    val distance = sqrt(dx * dx + dy * dy)
+
+    // Normalize the vector from the circle's center to the given point
+    val normalizedX = dx / distance
+    val normalizedY = dy / distance
+
+    // Scale the normalized vector by the circle's radius
+    val scaledX = normalizedX * radius
+    val scaledY = normalizedY * radius
+
+    // Add the scaled vector to the center of the circle
+    val closestX = circleCenter.x + scaledX
+    val closestY = circleCenter.y + scaledY
+
+    return Point(closestX.roundToInt(), closestY.roundToInt())
+}
+
+fun distance(p1: Point, p2: Point) = sqrt((p2.x - p1.x.toDouble()).pow(2) + (p2.y - p1.y.toDouble()).pow(2))
+
+fun shortestPath(circleCenters: List<Point>, radius: Int): List<Point> {
+    fun distance(p1: Point, p2: Point) = sqrt((p2.x - p1.x.toDouble()).pow(2) + (p2.y - p1.y.toDouble()).pow(2))
+    fun distanceDouble(p1: PointDouble, p2: PointDouble) = sqrt((p2.x - p1.x).pow(2) + (p2.y - p1.y).pow(2))
+
+    fun tangentPoints(
+        c1: PointDouble, c2: PointDouble, r: Int
+    ): Pair<Pair<PointDouble, PointDouble>, Pair<PointDouble, PointDouble>> {
+        val d = distanceDouble(c1, c2)
+        val alpha = PI / 2
+        val beta = atan2((c2.y - c1.y).toDouble(), (c2.x - c1.x).toDouble())
+        val gamma1 = beta + alpha
+        val gamma2 = beta - alpha
+
+        val tpA1 = PointDouble((c1.x + r * cos(gamma1)), (c1.y + r * sin(gamma1)))
+        val tpB1 = PointDouble((c2.x + r * cos(gamma1)), (c2.y + r * sin(gamma1)))
+
+        val tpA2 = PointDouble((c1.x + r * cos(gamma2)), (c1.y + r * sin(gamma2)))
+        val tpB2 = PointDouble((c2.x + r * cos(gamma2)), (c2.y + r * sin(gamma2)))
+
+        return ((tpA1 to tpB1) to (tpA2 to tpB2))
+    }
+
+    val pathPoints = mutableListOf<Point>()
+    if (circleCenters.size < 2) return pathPoints
+
+
+    for (i in circleCenters.indices) {
+        val circleA = circleCenters[i]
+        val circleB = circleCenters[(i + 1) % circleCenters.size]
+        val circleC = circleCenters[(i + 2) % circleCenters.size]
+
+        val (tangentAB1, tangentAB2) = tangentPoints(circleA.toPointDouble(), circleB.toPointDouble(), radius)
+        val (tangentBC1, tangentBC2) = tangentPoints(circleB.toPointDouble(), circleC.toPointDouble(), radius)
+
+        val listOfIntersections = listOf(
+            lineLineIntersection(tangentAB1.first, tangentAB1.second, tangentBC1.first, tangentBC1.second),
+            lineLineIntersection(tangentAB1.first, tangentAB1.second, tangentBC2.first, tangentBC2.second),
+            lineLineIntersection(tangentAB2.first, tangentAB2.second, tangentBC1.first, tangentBC1.second),
+            lineLineIntersection(tangentAB2.first, tangentAB2.second, tangentBC2.first, tangentBC2.second)
+        )
+        System.err.println(listOfIntersections.toString())
+        val closestIntersection = listOfIntersections.minBy {
+            distanceDouble(
+                it,
+                circleC.toPointDouble()
+            ) + distanceDouble(it, circleA.toPointDouble())
+        }
+        System.err.println(closestIntersection)
+        pathPoints.add(getClosestPointOnCircleToPoint(closestIntersection, circleB.toPointDouble(), radius))
+    }
+    System.err.println("Shortest Path: $pathPoints")
+    return pathPoints
+}
+
+fun lineLineIntersection(A: PointDouble, B: PointDouble, C: PointDouble, D: PointDouble): PointDouble {
+    // Line AB represented as a1x + b1y = c1
+    val a1 = (B.y - A.y)
+    val b1 = (A.x - B.x)
+    val c1 = a1 * A.x + b1 * A.y
+
+    // Line CD represented as a2x + b2y = c2
+    val a2 = (D.y - C.y)
+    val b2 = (C.x - D.x)
+    val c2 = a2 * C.x + b2 * C.y
+    val determinant = a1 * b2 - a2 * b1
+    return if (determinant == 0.0) {
+        // The lines are parallel. This is simplified
+        // by returning a pair of FLT_MAX
+        PointDouble(Double.MAX_VALUE, Double.MAX_VALUE)
+    } else {
+        val x = (b2 * c1 - b1 * c2) / determinant
+        val y = (a1 * c2 - a2 * c1) / determinant
+        PointDouble(x, y)
+    }
+}*/
